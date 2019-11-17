babee11561cdd458a4d65c60e0c9e19783601825
Isaac Velasquez <iav@princeton.edu>
2019-11-16 23:37:53 -0500

    removed mongo in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ var express = require('express');
 var ShareDB = require('sharedb');
 var WebSocket = require('ws');
 var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
-var MongoClient = require('mongodb').MongoClient,
-  Grid = MongoClient.Grid;
 
 var backend = new ShareDB();
 createDoc(startServer);
@@ -47,17 +45,4 @@ function startServer() {
   console.log('Listening on http://localhost:8080');
 }
 
-function put() {
-  // Connect to the db
-  MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-    if(err) return console.dir(err);
 
-    var grid = new Grid(db, 'fs');
-    var buffer = new Buffer(input);
-    grid.put(buffer, {metadata:{category:'text'}, content_type: 'text'}, function(err, fileInfo) {
-      if(!err) {
-        console.log("Finished writing file to Mongo");
-      }
-    });
-  });
-}
